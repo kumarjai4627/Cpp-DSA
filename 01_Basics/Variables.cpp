2bdd07b8ef5e2cd9511319e06034b3396e546165
Jai Kumar <your_email@gmail.com>
2026-07-06 22:23:20 +0530

    Initial commit

---
+++ 01_Basics/Variables.cpp
@@ -0,0 +1,28 @@
+/*
+Problem: Variables in C++
+Platform: Basics
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 20;
+    float height = 5.8;
+    char grade = 'A';
+    double pi = 3.1415926535;
+    bool isPassed = true;
+    string name = "Jai";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Value of PI: " << pi << endl;
+    cout << "Passed: " << isPassed << endl;
+
+    return 0;
+}
